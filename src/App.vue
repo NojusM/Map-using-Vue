85051e19620255386984e0648bec57201904a1dc
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-07-31 22:37:43 +0300

    CREATE VUE PROJECT THE RIGHT WAY

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <template>
-  <div>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>
