088c3591575b216b9b732c02186812b605f7dddc
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 20:18:37 +0300

    UPDATE BUTTON CSS

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -6,8 +6,15 @@
           <span class="label">Marker position: </span>
           <span class="coordinates">{{ marker[0] }}° N {{ marker[1] }}° E</span>
         </p>
-        <button class="btn delete" @click="markers.splice(index, 1)">Delete marker</button>
-        <button class="btn select" @click="selectedMarker = index">Select marker</button>
+        <div class="buttons">
+          <button
+            :class="['btn select', { active: index === selectedMarker }]"
+            @click="selectedMarker = index"
+          >
+            Select marker
+          </button>
+          <button class="btn delete" @click="markers.splice(index, 1)">Delete marker</button>
+        </div>
       </div>
       <button class="btn add" @click="markers.push(BASE_COORDINATES)">Add marker</button>
     </div>
@@ -87,5 +94,45 @@ const onMarkerDragEnd = (e: { target: { getLatLng: () => LatLng } }) => {
 
 .coordinates {
   font-style: italic;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+.btn {
+  width: 100%;
+  font-size: 1.5rem;
+  font-weight: 700;
+  padding: 0.5rem;
+  border: 2px solid black;
+  border-radius: 1rem;
+  cursor: pointer;
+  background-color: var(--primary-dark);
+  color: white;
+}
+
+.btn:hover {
+  background-color: var(--primary);
+}
+
+.btn.select.active {
+  background-color: var(--primary-bright);
+  color: black;
+}
+
+.btn.delete {
+  background-color: hsl(0, 90%, 20%);
+}
+
+.btn.delete:hover {
+  background-color: hsl(0, 90%, 40%);
+}
+
+.btn.add {
+  margin-top: 2rem;
 }
 </style>
