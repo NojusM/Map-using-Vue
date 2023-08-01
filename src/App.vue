84aa5347f5d0a64864f55d8228a40f204845465a
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 16:47:14 +0300

    CHANGE FROM SETUP TO OPTIONS API

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
+<script lang="ts">
+import { ref } from 'vue'
 import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  },
+  setup() {
+    const pages = ref(['About', 'Map', 'Hello'])
+    return { pages }
+  }
+}
 </script>
 
 <template>
-  <Navbar :pages="['About', 'Map']" />
+  <Navbar :pages="pages" />
   <main>
     <router-view />
   </main>
