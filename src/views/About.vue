3d4561377ec4a378d73c9b7c92032c5a15d6ee4f
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 15:34:57 +0300

    ADD DATA AND CSS TO ABOUT

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,48 @@
 <template>
-  <div>About</div>
+  <div class="wrapper">
+    <div class="about">
+      <div class="about-text">
+        <h1>What are we about</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quibusdam fugiat officiis,
+          voluptas explicabo corrupti exercitationem asperiores perspiciatis eligendi eaque
+          accusamus ratione doloremque dignissimos, animi quis pariatur nesciunt omnis aspernatur.
+        </p>
+      </div>
+      <img src="../assets/map.jpg" alt="about" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+}
+
+/* Use grid to avoid media queries */
+.about {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  gap: 2rem;
+  width: 70%;
+  max-width: 100%;
+  padding-top: 10rem;
+  overflow: hidden;
+  text-align: center;
+  align-items: center;
+}
+.about-text h1 {
+  font-size: 4rem;
+  font-weight: 600;
+}
+
+.about img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+</style>
