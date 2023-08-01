e1f6baba98a120fa7b48b218154cdcc751607f8c
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 19:08:39 +0300

    ADD ONCLICK TO ADD MARKER ON MAP

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -4,7 +4,7 @@
       <div class="marker-info">Marker position: {{ marker[0] }}° N {{ marker[1] }}° E</div>
     </div>
     <div class="map">
-      <l-map ref="map" v-model:zoom="zoom" v-model:center="center">
+      <l-map ref="map" v-model:zoom="zoom" v-model:center="center" @click="onMapClick">
         <l-control-layers position="bottomright"></l-control-layers>
         <l-tile-layer
           v-for="tileProvider in tileProviders"
@@ -25,13 +25,18 @@
 import 'leaflet/dist/leaflet.css'
 import tileProvidersData from '@/data/tileProviders.json'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import L, { type LatLngExpression, type PointExpression } from 'leaflet'
+import L, { LatLng, type LatLngExpression, type PointExpression } from 'leaflet'
 import { LMap, LTileLayer, LControlLayers, LMarker } from '@vue-leaflet/vue-leaflet'
 import { ref } from 'vue'
 const zoom = ref(7)
 const center = ref([54.6872, 25.2797] as PointExpression)
 const marker = ref([54.6872, 25.2797])
 const tileProviders = ref(tileProvidersData)
+
+const onMapClick = (e: { latlng: LatLng }) => {
+  const { lat, lng } = e.latlng
+  marker.value = [lat, lng]
+}
 </script>
 
 <style scoped>
