653e82ddbad44c4427f1b3b2aacfc0c9f3f220fc
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 21:39:20 +0300

    EXTRACT MARKER INTO COMPONENT

---
+++ src/components/Marker.vue
@@ -0,0 +1,34 @@
+<template>
+  <l-marker
+    v-for="(marker, index) in markerStore.markers"
+    :key="index"
+    :lat-lng="marker as LatLngExpression"
+    :icon="index === markerStore.selectedMarker ? SELECT_MARKER : DEFAULT_ICON"
+    draggable
+    @dragend="markerStore.addMarkerOnDrag"
+  ></l-marker>
+</template>
+
+<script setup lang="ts">
+import L, { type LatLngExpression } from 'leaflet'
+import { LMarker } from '@vue-leaflet/vue-leaflet'
+import { useMarkersStore } from '@/stores/markers'
+import CUSTOM_MARKER_ICON from '@/assets/custom-marker.png'
+import DEFAULT_MARKER_ICON from '@/assets/marker-icon.png'
+
+const SELECT_MARKER = L.icon({
+  iconUrl: CUSTOM_MARKER_ICON,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+})
+const DEFAULT_ICON = L.icon({
+  iconUrl: DEFAULT_MARKER_ICON,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+})
+
+const markerStore = useMarkersStore()
+</script>
+<style scoped></style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -2,7 +2,12 @@
   <div class="wrapper">
     <Sidebar />
     <div class="map">
-      <l-map ref="map" v-model:zoom="zoom" v-model:center="center" @click="onMapClick">
+      <l-map
+        ref="map"
+        v-model:zoom="zoom"
+        v-model:center="center"
+        @click="markerStore.addMarkerOnMap"
+      >
         <l-control-layers position="bottomright"></l-control-layers>
         <l-tile-layer
           v-for="tileProvider in tileProviders"
@@ -12,62 +17,28 @@
           :attribution="tileProvider.attribution"
           layer-type="base"
         />
-        <l-marker
-          v-for="(marker, index) in markers"
-          :key="index"
-          :lat-lng="marker as LatLngExpression"
-          :icon="index === selectedMarker ? SELECT_MARKER : DEFAULT_ICON"
-          draggable
-          @dragend="onMarkerDragEnd"
-        ></l-marker>
+        <Marker />
       </l-map>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import 'leaflet/dist/leaflet.css'
-import CUSTOM_MARKER_ICON from '@/assets/custom-marker.png'
-import DEFAULT_MARKER_ICON from '@/assets/marker-icon.png'
-import tileProvidersData from '@/data/tileProviders.json'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import L, { LatLng, type LatLngExpression, type PointExpression } from 'leaflet'
-import { LMap, LTileLayer, LControlLayers, LMarker } from '@vue-leaflet/vue-leaflet'
+import L, { type PointExpression } from 'leaflet'
+import { LMap, LTileLayer, LControlLayers } from '@vue-leaflet/vue-leaflet'
+import { ref } from 'vue'
+import { useMarkersStore } from '@/stores/markers'
 import Sidebar from '@/components/Sidebar.vue'
-import { ref, type Ref } from 'vue'
-import truncateFloat from '@/composibles/truncateFloat'
+import Marker from '@/components/Marker.vue'
+import tileProvidersData from '@/data/tileProviders.json'
+import 'leaflet/dist/leaflet.css'
 const BASE_COORDINATES: [number, number] = [55.23479, 23.92822]
-const SELECT_MARKER = L.icon({
-  iconUrl: CUSTOM_MARKER_ICON,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34]
-})
-const DEFAULT_ICON = L.icon({
-  iconUrl: DEFAULT_MARKER_ICON,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34]
-})
+
+const markerStore = useMarkersStore()
 const zoom = ref(7)
 const center = ref(BASE_COORDINATES as PointExpression)
 const tileProviders = ref(tileProvidersData)
-const markers: Ref<[number, number][]> = ref([])
-const selectedMarker = ref(0)
-
-const onMapClick = (e: { latlng: LatLng }) => {
-  const { lat, lng } = e.latlng
-  const truncatedLat = truncateFloat(lat, 5)
-  const truncatedLng = truncateFloat(lng, 5)
-  markers.value[selectedMarker.value] = [truncatedLat, truncatedLng]
-}
-
-const onMarkerDragEnd = (e: { target: { getLatLng: () => LatLng } }) => {
-  const { lat, lng } = e.target.getLatLng()
-  const truncatedLat = truncateFloat(lat, 5)
-  const truncatedLng = truncateFloat(lng, 5)
-  markers.value[selectedMarker.value] = [truncatedLat, truncatedLng]
-}
 </script>
 
 <style scoped>
