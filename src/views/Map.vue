557a55f76a6671941acd782ace1d543893df450a
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 18:02:17 +0300

    CREATE BASIC MAP

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>Map</div>
+  <div style="height: 600px; width: 800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
 </template>
+
+<script lang="ts">
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+
+export default {
+  components: {
+    LMap,
+    LTileLayer
+  },
+  data() {
+    return {
+      zoom: 2
+    }
+  }
+}
+</script>
+
+<style></style>
