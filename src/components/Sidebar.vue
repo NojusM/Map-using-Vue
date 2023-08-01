616d6a99ac5a0176da6fb71fd35475d4ca3c2c45
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 21:08:36 +0300

    EXTRACT SIDEBAR

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="sidebar">
+    <div v-for="(marker, index) in props.markers.value" :key="index" class="marker-info">
+      <p>
+        <span class="label">Marker position: </span>
+        <span class="coordinates">{{ marker[0] }}° N {{ marker[1] }}° E</span>
+      </p>
+      <div class="buttons">
+        <button
+          :class="['btn select', { active: index === selectedMarker.value }]"
+          @click="selectedMarker = index"
+        >
+          Select marker
+        </button>
+        <button class="btn delete" @click="markers.splice(index, 1)">Delete marker</button>
+      </div>
+    </div>
+    <button class="btn add" @click="markers.push(BASE_COORDINATES)">Add marker</button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.sidebar {
+  font-size: 2rem;
+  padding: 1rem;
+}
+
+.label {
+  font-weight: 700;
+}
+
+.coordinates {
+  font-style: italic;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+.btn {
+  width: 100%;
+  font-size: 1.5rem;
+  font-weight: 700;
+  padding: 0.5rem;
+  border: 2px solid black;
+  border-radius: 1rem;
+  cursor: pointer;
+  background-color: var(--primary-dark);
+  color: white;
+}
+
+.btn:hover {
+  background-color: var(--primary);
+}
+
+.btn.select.active {
+  background-color: hsl(120, 47%, 55%);
+  color: black;
+}
+
+.btn.delete {
+  background-color: hsl(0, 90%, 20%);
+}
+
+.btn.delete:hover {
+  background-color: hsl(0, 90%, 40%);
+}
+
+.btn.add {
+  margin-top: 2rem;
+}
+</style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="wrapper">
-    <div class="sidebar">
-      <div v-for="(marker, index) in markers" :key="index" class="marker-info">
-        <p>
-          <span class="label">Marker position: </span>
-          <span class="coordinates">{{ marker[0] }}° N {{ marker[1] }}° E</span>
-        </p>
-        <div class="buttons">
-          <button
-            :class="['btn select', { active: index === selectedMarker }]"
-            @click="selectedMarker = index"
-          >
-            Select marker
-          </button>
-          <button class="btn delete" @click="markers.splice(index, 1)">Delete marker</button>
-        </div>
-      </div>
-      <button class="btn add" @click="markers.push(BASE_COORDINATES)">Add marker</button>
-    </div>
+    <Sidebar :markers="markers" :selected-marker="selectedMarker" />
     <div class="map">
       <l-map ref="map" v-model:zoom="zoom" v-model:center="center" @click="onMapClick">
         <l-control-layers position="bottomright"></l-control-layers>
@@ -50,6 +33,7 @@ import tileProvidersData from '@/data/tileProviders.json'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import L, { LatLng, type LatLngExpression, type PointExpression } from 'leaflet'
 import { LMap, LTileLayer, LControlLayers, LMarker } from '@vue-leaflet/vue-leaflet'
+import Sidebar from '@/components/Sidebar.vue'
 import { ref, type Ref } from 'vue'
 import truncateFloat from '@/composibles/truncateFloat'
 const BASE_COORDINATES: [number, number] = [55.23479, 23.92822]
@@ -101,58 +85,5 @@ const onMarkerDragEnd = (e: { target: { getLatLng: () => LatLng } }) => {
 .leaflet-container {
   font-size: 1.5rem;
   border: 1px solid black;
-}
-
-.sidebar {
-  font-size: 2rem;
-  padding: 1rem;
-}
-
-.label {
-  font-weight: 700;
-}
-
-.coordinates {
-  font-style: italic;
-}
-
-.buttons {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-}
-.btn {
-  width: 100%;
-  font-size: 1.5rem;
-  font-weight: 700;
-  padding: 0.5rem;
-  border: 2px solid black;
-  border-radius: 1rem;
-  cursor: pointer;
-  background-color: var(--primary-dark);
-  color: white;
-}
-
-.btn:hover {
-  background-color: var(--primary);
-}
-
-.btn.select.active {
-  background-color: hsl(120, 47%, 55%);
-  color: black;
-}
-
-.btn.delete {
-  background-color: hsl(0, 90%, 20%);
-}
-
-.btn.delete:hover {
-  background-color: hsl(0, 90%, 40%);
-}
-
-.btn.add {
-  margin-top: 2rem;
 }
 </style>
