3e231cb90a066c45d50f40a1d4104a6c74e9e663
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-08-01 18:44:54 +0300

    ADD MARKER AND REFACTOR TO SETUP

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -12,50 +12,42 @@
           :attribution="tileProvider.attribution"
           layer-type="base"
         />
+        <l-marker :lat-lng="marker"></l-marker>
       </l-map>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import 'leaflet/dist/leaflet.css'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import L, { type PointExpression } from 'leaflet'
-import { LMap, LTileLayer, LControlLayers } from '@vue-leaflet/vue-leaflet'
+import L, { LatLng, type LatLngExpression, type PointExpression } from 'leaflet'
+import { LMap, LTileLayer, LControlLayers, LMarker } from '@vue-leaflet/vue-leaflet'
+import { ref } from 'vue'
 
-export default {
-  components: {
-    LMap,
-    LTileLayer,
-    LControlLayers
+const zoom = ref(7)
+const center = ref([54.6872, 25.2797] as PointExpression)
+const marker = ref([54.6872, 25.2797] as LatLngExpression)
+const tileProviders = ref([
+  {
+    name: 'OpenStreetMap',
+    attribution:
+      '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
   },
-  data() {
-    return {
-      zoom: 7,
-      center: [54.6872, 25.2797] as PointExpression,
-      tileProviders: [
-        {
-          name: 'OpenStreetMap',
-          attribution:
-            '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-          url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        },
-        {
-          name: 'Stadia Maps',
-          attribution:
-            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-          url: 'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png'
-        },
-        {
-          name: 'CartoDB_Voyager',
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-          url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
-        }
-      ]
-    }
+  {
+    name: 'Stadia Maps',
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    url: 'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png'
+  },
+  {
+    name: 'CartoDB_Voyager',
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
   }
-}
+])
 </script>
 
 <style scoped>
